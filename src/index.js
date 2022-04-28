0c52e65b74d8caf4e25913240cc4094815a3d91e
Talha <tkz9696@gmail.com>
2022-04-28 21:09:04 +0500

    Delete and Add functionality added

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import './styles/main.css';
+import { addTask, editTasks } from './Tasks.js';
 
-const tasks = [
-  {
-    description: 'do laundry',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'do dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'eat pizza',
-    completed: false,
-    index: 2,
-  },
-];
+const addBtn = document.getElementById('add-btn');
+const todoListUL = document.querySelector('.todo-list');
 
-const todoList = document.querySelector('.todo-list');
+addBtn.addEventListener('click', addTask);
 
-const populateList = () => {
-  tasks.forEach((task) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<input type="checkbox"><div class="task-text">${task.description}</div><i class="fas fa-ellipsis-v"></i>`;
-    todoList.appendChild(li);
-  });
-};
-
-populateList();
+todoListUL.addEventListener('focusout', (e) => editTasks(e));
