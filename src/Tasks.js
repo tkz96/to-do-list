9a787ad8fc6e4e9ff779af1878b582da94eb11ea
talha <tkz9696@gmail.com>
2022-04-29 14:43:52 +0500

    List interactivity added

--- src/Tasks.js
+++ src/Tasks.js
@@ -14,8 +14,8 @@ const populateList = (tasksArray) => {
   todoList.innerHTML = '';
   for (let i = 0; i < tasksArray.length; i += 1) {
     const li = document.createElement('li');
-    li.id = `${tasksArray[i].index}`;
-    li.innerHTML = `<input type="checkbox"><div class="task-text" contenteditable="true" data_id='${i}'>${tasksArray[i].description}</div>
+    //  li.id = `${tasksArray[i].index}`;
+    li.innerHTML = `<input type="checkbox" class="checkbox" data_id='${i}' ${tasksArray[i].completed ? 'checked' : ''}><div class="task-text ${tasksArray[i].completed ? 'line' : ''}" contenteditable="true" data_id='${i}'>${tasksArray[i].description}</div>
         <i class="fas fa-trash-alt delete" data_id='${i}'></i>`;
     todoList.appendChild(li);
   }
@@ -60,6 +60,26 @@ export const editTasks = (e) => {
     tasksArray[e.target.getAttribute('data_id')].description = e.target.innerText;
     updateLS(tasksArray);
   }
+};
+
+export const check = (e) => {
+  if (e.target.className.includes('checkbox')) {
+    const { checked } = e.target;
+    const text = e.target.parentNode.querySelector('.task-text');
+    if (checked) {
+      text.classList.add('line');
+    } else {
+      text.classList.remove('line');
+    }
+    tasksArray[e.target.getAttribute('data_id')].completed = checked;
+  }
+  updateLS(tasksArray);
+};
+
+export const clear = () => {
+  tasksArray = tasksArray.filter((task) => (task.completed === false));
+  updateLS(tasksArray);
+  populateList(tasksArray);
 };
 
 populateList(tasksArray);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './styles/main.css';
-import { addTask, editTasks } from './Tasks.js';
+import {
+  addTask, editTasks, check, clear,
+} from './Tasks.js';
 
 const addBtn = document.getElementById('add-btn');
 const todoListUL = document.querySelector('.todo-list');
+const clearBtn = document.getElementById('clear-btn');
 
-addBtn.addEventListener('click', addTask);
+window.addEventListener('DOMContentLoaded', () => {
+  addBtn.addEventListener('click', addTask);
 
-todoListUL.addEventListener('focusout', (e) => editTasks(e));
+  todoListUL.addEventListener('focusout', (e) => editTasks(e));
+  todoListUL.addEventListener('change', (e) => check(e));
+
+  clearBtn.addEventListener('click', clear);
+});
